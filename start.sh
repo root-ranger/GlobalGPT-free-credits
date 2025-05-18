744bf8ad8728e72d744e39527cb194e39d26e8dd
root-ranger <root.ranger@proton.me>
2025-05-18 18:30:14 +0530

    Updated start.sh to echo messages

--- start.sh
+++ start.sh
@@ -1,12 +1,24 @@
 python3 main.py &&
+echo "Done 1" &&
 python3 main.py &&
+echo "Done 2" &&
 python3 main.py &&
+echo "Done 3" &&
 python3 main.py &&
+echo "Done 4" &&
 python3 main.py &&
+echo "Done 5" &&
 python3 main.py &&
+echo "Done 6" &&
 python3 main.py &&
+echo "Done 7" &&
 python3 main.py &&
+echo "Done 8" &&
 python3 main.py &&
+echo "Done 9" &&
 python3 main.py &&
+echo "Done 10" &&
 python3 main.py &&
+echo "Done 11" &&
 python3 main.py
+echo "Done 12" &&
